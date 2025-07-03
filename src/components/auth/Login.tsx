2d4152398f932bb21081496d2333c8dccf440b1d
Gustin Rheza <rezarna4@gmail.com>
2025-07-03 23:58:31 +0700

    login hooks

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -2,17 +2,21 @@ import React from 'react'
 import Auth from './Auth'
 import { Link } from 'react-router-dom'
 import { Link as MUILink } from '@mui/material'
+import { useLogin } from '../../hooks/useLogin'
 
 const Login = () => {
+    const {login, error} = useLogin()
+
     return (
         <>
-            <Auth submitLabel={'Login'} onSubmit={async () => { }} >
+            <Auth submitLabel={'Login'} onSubmit={async (request) => { login(request)}} >
                 <p className='text-center'>
                     Do not have an account?{' '}
                     <MUILink component={Link} to="/signup">
                         Sign Up
                     </MUILink>
                 </p>
+                {error && <p className='text-red-500 text-center'>Login failed. Please try again.</p>}
             </Auth>
         </>
     )
